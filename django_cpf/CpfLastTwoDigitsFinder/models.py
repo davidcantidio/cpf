91c69ceb066dadedcb28267008e0349bbcf47e5a
David Cantidio <dbcantidio@gmail.com>
2022-11-25 19:41:15 -0300

    Logic working via user input in terminal

--- django_cpf/CpfLastTwoDigitsFinder/models.py
+++ django_cpf/CpfLastTwoDigitsFinder/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cpf(models.Model):
+    nine_digit_cpf = models.IntegerField(max_length=9)
+    def __str__(self):
+        return f"{self.nine_digit_cpf}"
+    
--- django_cpf/CpfLastTwoDigitsFinder/views.py
+++ django_cpf/CpfLastTwoDigitsFinder/views.py
@@ -1,5 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def get10DigCPF(nine_dig_cpf):
+    n_digits = list(range(2, 11, 1))
+    n_digits.reverse()
+    i = 0
+    l = list()
+
+    for n in nine_dig_cpf:
+        multiplied = int(n) * n_digits[i]
+        l.append(multiplied)
+        i += 1
+                        
+    l = sum(l)
+    r = l % 11
+    if r == 0 or r == 1:
+        tenth_digit = 0
+    else: 
+        tenth_digit = 11 - r
+
+    ten_dig_cpf = nine_dig_cpf + str(tenth_digit)
+
+    return(ten_dig_cpf)
+
+
+def get11DigCPF(ten_dig_cpf):
+    n_digits = list(range(2, 12, 1))
+    n_digits.reverse()
+    i = 0
+    l = list()
+    for n in ten_dig_cpf:
+        multiplied = int(n) * n_digits[i]
+        i += 1
+        l.append(multiplied)
+    l = sum(l)
+    r = l % 11
+    if r == 0 or r == 1:
+        eleventh_digit = 0
+    else: 
+        eleventh_digit = 11 - r
+
+    eleven_dig_cpf = ten_dig_cpf + str(eleventh_digit)
+
+    return(eleven_dig_cpf)
+
+
+
+
 def home(request):
-    return  render(request, 'CpfLastTwoDigitsFinder/home.html')
+    
+    cpf_input = request.POST.get('cpf')
+    ten_dig = get10DigCPF(cpf_input)
+    eleven_dig_cpf = get11DigCPF(ten_dig)
+    last_two_digits = eleven_dig_cpf[-2:]
+    context = {'last_two_digits':last_two_digits}
+    return  render(request, 'CpfLastTwoDigitsFinder/home.html', context)
